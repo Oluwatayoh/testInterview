e4b47e3f9d7b6748bff355540f8b9e270186c52b
Oluwatayoh <ojotayoh2@gmail.com>
2019-05-22 22:53:38 +0100

    upddate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from './material-module';
+import { Section1Component } from './section1/section1.component';
+import { TopbarComponent } from './section1/topbar/topbar.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
+import { Section4Component } from './section4/section4.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Section1Component,
+    TopbarComponent,
+    Section2Component,
+    Section3Component,
+    Section4Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
