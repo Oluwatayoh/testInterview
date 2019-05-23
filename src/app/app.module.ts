eeb57ddc309552891d6ff8118257665bbf6ab4df
Oluwatayoh <ojotayoh2@gmail.com>
2019-05-23 20:37:05 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,12 @@ import { TopbarComponent } from './section1/topbar/topbar.component';
 import { Section2Component } from './section2/section2.component';
 import { Section3Component } from './section3/section3.component';
 import { Section4Component } from './section4/section4.component';
+import { Section5Component } from './section5/section5.component';
+import { Section6Component } from './section6/section6.component';
+import { Section7Component } from './section7/section7.component';
+import { Section8Component } from './section8/section8.component';
+import { Section9Component } from './section9/section9.component';
+import { Section10Component } from './section10/section10.component';
 
 
 @NgModule({
@@ -20,7 +26,13 @@ import { Section4Component } from './section4/section4.component';
     TopbarComponent,
     Section2Component,
     Section3Component,
-    Section4Component
+    Section4Component,
+    Section5Component,
+    Section6Component,
+    Section7Component,
+    Section8Component,
+    Section9Component,
+    Section10Component
   ],
   imports: [
     BrowserModule,
